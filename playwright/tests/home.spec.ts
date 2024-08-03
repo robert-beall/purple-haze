563a4d9471fb68a6551edd26c880dbad5c08b778
rbeall@smcm.edu <>
2024-08-02 22:47:39 -0400

    Add homepage test cleanup playwright code

--- playwright/tests/home.spec.ts
+++ playwright/tests/home.spec.ts
@@ -1,6 +1,29 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import 'dotenv/config';
 
-test('title', async ({ page }) => {
-    await page.goto(`${process.env.FRONTEND_URL}`);
+/**
+ * Tests for page content.
+ */
+test.describe(() => {
+    /**
+     * A simple test for testing the page heading.
+     */
+    test('heading', async ({ page }) => {
+        await page.goto(`${process.env.FRONTEND_URL}`);
+    
+        const heading = page.getByTestId('heading');
+        expect(await heading.textContent()).toBe('Purple Haze');
+        expect(heading).toBeVisible();
+    });
+
+    /**
+     * A simple test for testing the page subheading.
+     */
+    test('subheading', async ({ page }) => {
+        await page.goto(`${process.env.FRONTEND_URL}`);
+    
+        const subheading = page.getByTestId('subheading');
+        expect(await subheading.textContent()).toBe('A Simple and Fearless Playwright Testbed');
+        expect(subheading).toBeVisible();
+    });
 });
